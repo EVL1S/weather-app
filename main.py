038ef346cf441ffef2fe5c7b1e26d809a234f9a8
Kubob <Tunndr@gmail.com>
2025-10-23 21:56:24 +0200

    App is connected to API, Shows city, date/time, wind, apparent temp and weather for 3 days

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #Weather App
-
+from datetime import datetime, timedelta
 import requests
+import locale
+from wmo import wmo as WMO 
 
 
 userInput_city = input('Zadej město: ')
@@ -14,19 +16,51 @@ if "results" not in data:
 
 lat = data["results"][0]["latitude"]
 lon = data["results"][0]["longitude"]
+locale.setlocale(locale.LC_TIME, "cs_CZ.UTF-8")
 
 url_weather = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current=temperature_2m,wind_speed_10m,apparent_temperature&daily=temperature_2m_min,temperature_2m_max,weather_code&forecast_days=3&timezone=Europe/Prague"
 weather_response = requests.get(url_weather)
 weather_data = weather_response.json()
 
 temp = weather_data["current"]["temperature_2m"]
+apparent = weather_data["current"]["apparent_temperature"]
 temp_unit = weather_data["current_units"]["temperature_2m"]
+temp_min = weather_data['daily']['temperature_2m_min']
+temp_max = weather_data['daily']['temperature_2m_max']
 wind_speed = weather_data["current"]["wind_speed_10m"]
 wind_unit = weather_data["current_units"]["wind_speed_10m"]
+weather_code = weather_data['daily']['weather_code']
+timezone = weather_data['timezone']
+timezone_abbreviation = weather_data['timezone_abbreviation']
+time = datetime.fromisoformat(weather_data['current']['time'])
+next_day = (time + timedelta(days=1))
+day_after = (time + timedelta(days=2))
+elevation = weather_data['elevation']
+wmo0 = WMO.get(int(weather_code[0]), f"Neznámý jev (kód: {weather_code[0]})")
+wmo1 = WMO.get(int(weather_code[1]), f"Neznámý jev (kód: {weather_code[1]})")
+wmo2 = WMO.get(int(weather_code[2]), f"Neznámý jev (kód: {weather_code[2]})")
+
+print("\n", weather_data, "\n")
+
+def printWeather():
+    return f"""
+🌦️  Počasí pro: {userInput_city}
+📍 Souřadnice: {lat}, {lon}     🗺️  Časová zóna: {timezone} ({timezone_abbreviation})
+---------------------------------------------------------------------------
+
+🕒 Aktuálně - {time.strftime("%d. %B %Y, %H:%M")}
+🌡️  Teplota: {temp} {temp_unit}  {"🥶" if apparent < temp else "🥵"} Pocitová teplota: {apparent} {temp_unit}
+{wmo0}
+💨 Vítr: {wind_speed} {wind_unit}
+🏔️  Nadmořská výška: {round(elevation)} m
+
+📅 Předpověď na 3 dny
+---------------------------------------------------------------------------
+🗓️  {time.strftime("%A")[:2]} {time.strftime("%d.%m.")}   🌦️  min: {temp_min[0]} {temp_unit}   max: {temp_max[0]} {temp_unit}   {wmo0}
+🗓️  {next_day.strftime("%A")[:2]} {next_day.strftime("%d.%m.")}   🌧️  min: {temp_min[1]} {temp_unit}   max: {temp_max[1]} {temp_unit}   {wmo1}
+🗓️  {day_after.strftime("%A")[:2]} {day_after.strftime("%d.%m.")}   🌩️  min: {temp_min[2]} {temp_unit}   max: {temp_max[2]} {temp_unit}   {wmo2}
+---------------------------------------------------------------------------
+"""
 
 
-def getWeather(c, t, tu, w, wu):
-    return f"Počasí pro {c}\n{t} {tu}\n{w} {wu}"
-
-
-print(getWeather(userInput_city, temp, temp_unit, wind_speed, wind_unit))
+print(printWeather())
---
+++ wmo.py
@@ -0,0 +1,36 @@
+wmo = {
+    0:  "☀️  Jasno",
+    1:  "🌤️  Převážně jasno",
+    2:  "⛅  Polojasno",
+    3:  "☁️  Zataženo",
+    45: "🌫️  Mlha",
+    48: "🌫️  Námrazová mlha",
+
+    51: "🌦️  Slabé mrholení",
+    53: "🌦️  Mírné mrholení",
+    55: "🌦️  Silné mrholení",
+    56: "🧊🌧️  Slabé mrznoucí mrholení",
+    57: "🧊🌧️  Silné mrznoucí mrholení",
+
+    61: "🌧️  Slabý déšť",
+    63: "🌧️  Mírný déšť",
+    65: "🌧️  Silný déšť",
+    66: "🧊🌧️  Slabý mrznoucí déšť",
+    67: "🧊🌧️  Silný mrznoucí déšť",
+
+    71: "🌨️  Slabé sněžení",
+    73: "🌨️  Mírné sněžení",
+    75: "🌨️  Silné sněžení",
+    77: "❄️  Sněhová zrna",
+
+    80: "🌦️  Slabé přeháňky",
+    81: "🌦️  Mírné přeháňky",
+    82: "🌧️  Prudké přeháňky",
+
+    85: "🌨️  Slabé sněhové přeháňky",
+    86: "🌨️  Silné sněhové přeháňky",
+
+    95: "⛈️  Bouřka",
+    96: "⛈️🌩️  Bouřka s mírnými kroupami",
+    99: "⛈️🌩️  Bouřka se silnými kroupami"
+}
